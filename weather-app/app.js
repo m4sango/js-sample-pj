1ea42432c30d8c3fcf712e00b3613aa2bb94db7a
m4sango <m.s.1022.ttlove@gmail.com>
2020-04-12 16:17:56 +0900

    wip: weather app sample

---
+++ weather-app/app.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const api = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+    });
+  }
+});
