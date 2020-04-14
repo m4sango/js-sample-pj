b505b28799d761056d1efb7de46b4666d41c7420
m4sango <m.s.1022.ttlove@gmail.com>
2020-04-15 02:47:41 +0900

    update: boards page

--- torrelo-clone/src/components/boards/BoardCreateDialog.js
+++ torrelo-clone/src/components/boards/BoardCreateDialog.js
@@ -1,14 +1,44 @@
-import React from "react";
-
+import React, { useState } from "react";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
+import {
+  makeStyles,
+  Button,
+  TextField,
+  DialogContent,
+  DialogActions,
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+
+const useStyles = makeStyles({
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    alignContent: "space-between",
+  },
+  closeBtn: {
+    display: "flex",
+    flexDirection: "row",
+    cursor: "pointer",
+    margin: "0",
+  },
+  title: {
+    fontSize: "2px",
+  },
+  content: {},
+});
 
 export default function BoardCreateDialog(props) {
+  const classes = useStyles();
+
   const { onClose, open, onCreate } = props;
+  const [disabled, setDisabled] = useState(true);
   let inputValue = "";
 
   const handleOnChangesValue = (e) => {
-    inputValue += e.target.value;
+    inputValue = e.target.value;
+
+    setDisabled(!inputValue);
   };
 
   const handleOnCreate = (e) => {
@@ -20,21 +50,33 @@ export default function BoardCreateDialog(props) {
 
   return (
     <Dialog onClose={onClose} aria-labelledby="simple-dialog-title" open={open}>
-      <div className="board-create-dialog-header">
-        <DialogTitle id="新しいボードを作成">Set backup account</DialogTitle>
-        <button type="button" className="dialog-close-btn">
-          <span className="dialog-close-btn-icon">X</span>
-        </button>
+      <div className={classes.header}>
+        <DialogTitle id="form-dialog-title" className={classes.title}>
+          新しいボードを作成
+        </DialogTitle>
+        <Button className={classes.closeBtn}>
+          <CloseIcon></CloseIcon>
+        </Button>
       </div>
-      <form className="board-create-dialog-form">
-        <input
+      <DialogContent className={classes.content}>
+        <TextField
+          className={classes.formTextField}
+          required
+          label="required"
           placeholder="ボードタイトルを追加"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          autoFocus
+          fullWidth
           onChange={handleOnChangesValue}
-        ></input>
-        <button type="submit" onClick={handleOnCreate}>
-          <span>ボードを作成</span>
-        </button>
-      </form>
+        />
+        <DialogActions>
+          <Button onClick={handleOnCreate} color="primary" disabled={disabled}>
+            ボードを作成
+          </Button>
+        </DialogActions>
+      </DialogContent>
     </Dialog>
   );
 }
--- torrelo-clone/src/pages/boards/Boards.js
+++ torrelo-clone/src/pages/boards/Boards.js
@@ -1,13 +1,58 @@
 import React, { useState } from "react";
-import Board from "../../components/boards/Board";
 import "./Boards.css";
 import BoardCreateDialog from "../../components/boards/BoardCreateDialog";
 import uuid from "react-uuid";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+const useStyles = makeStyles({
+  root: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+  boardList: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    bottom: 0,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardCommon: {
+    display: "flex",
+    width: "20vw",
+    minWidth: "150px",
+    height: "10vh",
+    minHeight: "100px",
+    backgroundColor: "lightblue",
+  },
+  cardRoot: {
+    margin: "8px",
+  },
+  cardContent: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardTitle: {
+    alignItems: "center",
+    color: "white",
+  },
+});
 
 export default function Boards() {
+  const classes = useStyles();
+
   const [boards, setBoards] = useState([]);
   const [open, setOpen] = useState(false);
-  //   setBoards({ title: "テスト", id: "abcd" });
 
   const handleOnCreate = (title) => {
     const list = boards.slice();
@@ -25,20 +70,24 @@ export default function Boards() {
   };
 
   return (
-    <div className="boards-wrapper">
-      <ul className="board-section-list">
+    <div className={classes.root}>
+      <div className={classes.boardList}>
         {boards.map((board, index) => (
           <Board key={`${index}-${board.title}`} board={board}></Board>
         ))}
-        <li className="board-section-list-item">
-          <div
-            className="board-tile board-create"
+        <Card className={`${classes.cardCommon} ${classes.cardRoot}`}>
+          <CardActionArea
+            className={classes.cardCommon}
             onClick={handleOnClickCreateBoard}
           >
-            <p>新しいボードを作成</p>
-          </div>
-        </li>
-      </ul>
+            <CardContent className={classes.cardContent}>
+              <Typography className={classes.cardTitle}>
+                新しいボードを作成
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
       <BoardCreateDialog
         open={open}
         onClose={handleDialogClose}
@@ -47,3 +96,24 @@ export default function Boards() {
     </div>
   );
 }
+
+const Board = (props) => {
+  const classes = useStyles();
+  const board = props.board;
+
+  return (
+    <Card className={`${classes.cardCommon} ${classes.cardRoot}`}>
+      <CardActionArea
+        className={classes.cardCommon}
+        component={Link}
+        to={`/b/${board.id}/${board.title}`}
+      >
+        <CardContent className={classes.cardContent}>
+          <Typography className={classes.cardTitle} variant="h5">
+            {board.title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
